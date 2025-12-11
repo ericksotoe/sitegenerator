53820bfa49f7483963de05d0546814353fa10336
ericksotoe <ericksotoe@gmail.com>
2025-12-11 11:09:03 +0900

    Testing if the docs are displayed on the github webpage

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from copystatic import copy_directory_contents, generate_pages_recursive
 
 
 def main():
+    # default to root if no arg is passed
     basepath = "/"
     if len(sys.argv) > 1:
         basepath = sys.argv[1]
